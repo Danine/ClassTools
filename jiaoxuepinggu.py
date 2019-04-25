39633861216c4571a44e72819ace12af15332238
Tyson <381980920@qq.com>
2019-04-26 01:42:39 +0800

    success!

--- jiaoxuepinggu.py
+++ jiaoxuepinggu.py
@@ -3,8 +3,8 @@ from selenium.webdriver.common.action_chains import ActionChains
 import time
 driver = webdriver.Chrome()
 driver.get("http://bkjws.sdu.edu.cn/")
-driver.find_element_by_name("j_username").send_keys("201618130133")
-driver.find_element_by_name("j_password").send_keys("110520")
+driver.find_element_by_name("j_username").send_keys("201600130069")
+driver.find_element_by_name("j_password").send_keys("123456")
 driver.find_element_by_id("loginButtonId").click()
 
 driver.implicitly_wait(10)
@@ -18,7 +18,8 @@ time.sleep(1)
 temp = driver.find_elements_by_tag_name('tbody')[2].find_elements_by_tag_name('tr')
 row = len(temp)
 for j in range(row):
-    if driver.find_element_by_xpath("//*[@id='dataTableId']/tbody/tr["+str(j+1)+"]/td[4]/font") != 0:
+    # if driver.find_element_by_xpath("//*[@id='dataTableId']/tbody/tr["+str(j+1)+"]/td[4]/font") != 1:
+    if not driver.find_element_by_xpath("//*[@id='dataTableId']/tbody/tr["+str(j+1)+"]/td[4]/font").find_elements_by_tag_name('a'):
         driver.find_element_by_xpath("//*[@id='dataTableId']/tbody/tr["+str(j+1)+"]/td[5]/a").click()
         for i in range(19):
             driver.find_elements_by_id('zbda_'+str(i))[0].click()
@@ -26,25 +27,14 @@ for j in range(row):
         driver.find_elements_by_id('zbda_19')[2].click()
         driver.find_elements_by_id('zbda_20')[0].click()
         driver.find_element_by_id('zbda_21').send_keys("老师讲课思路清晰，认真负责")
-        submit = driver.find_elements_by_class_name('red')[0]
-        ActionChains(driver).
-        submit.click()
+        js="window.scrollTo(0,0)"
+        driver.execute_script(js)
+        driver.find_elements_by_class_name('red')[0].click()
         driver.find_element_by_xpath("/html/body/div[1]/div/table/tbody/tr[2]/td[2]/div/table/tbody/tr[3]/td/div/button[1]").click()
+        time.sleep(2)
+        driver.find_element_by_xpath("/html/body/div[1]/div/table/tbody/tr[2]/td[2]/div/table/tbody/tr[3]/td/div/button").click()
+        time.sleep(1)
+        driver.find_element_by_xpath('//*[@id="fhButtonId"]').click()
     else:
         continue
-time.sleep(5)
-
-
-# table = driver.find_elements_by_id('dataTableId')
-# for i in table:
-#     table_tr = i.find_element_by_tag_name('tr')
-#     if table_tr.find_element_by_tag_name('td')[4].txt == 0:
-#         table_tr.find_element_by_tag_name('td')[5].click()
-#     else:
-#         print(table_tr.find_element_by_tag_name('td')[4].txt)
-#     if driver.find_element_by_xpath("//*[@id='dataTableId']/tbody/tr[1]/td[4]/font") == 0:
-#         driver.find_element_by_xpath("//*[@id='dataTableId']/tbody/tr[1]/td[5]/a").click()
-
-#     if i.value == "5.0":
-#         i.click()
-#         time.sleep(5)
+    time.sleep(3)
